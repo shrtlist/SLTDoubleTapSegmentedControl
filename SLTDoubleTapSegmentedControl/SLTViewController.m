72f0ebcb3431af9fde9fc5e85c81f24b57e1011c
Marco Abundo <marco.abundo@rallyhealth.com>
2018-02-22 20:18:19 -0800

    Reverted to Objective-C

---
+++ SLTDoubleTapSegmentedControl/SLTViewController.m
@@ -0,0 +1,29 @@
+//
+//  SLTViewController.m
+//  SLTDoubleTapSegmentedControl
+//
+//  Created by Marco Abundo on 3/11/14.
+//  Copyright (c) 2014 shrtlist. All rights reserved.
+//
+
+#import "SLTViewController.h"
+#import "SLTDoubleTapSegmentedControl.h"
+
+@interface SLTViewController () // Class extension
+@property (nonatomic, weak) IBOutlet SLTDoubleTapSegmentedControl *doubleTapSegmentedControl;
+@end
+
+@implementation SLTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+
+    [self.doubleTapSegmentedControl setTintColor:[UIColor greenColor] forSegmentAtIndex:0];
+    [self.doubleTapSegmentedControl setTintColor:[UIColor redColor] forSegmentAtIndex:1];
+}
+
+#pragma mark - Private methods
+
+@end
---
+++ SLTDoubleTapSegmentedControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SLTDoubleTapSegmentedControl
+//
+//  Created by Marco Abundo on 2/28/14.
+//  Copyright (c) 2014 shrtlist. All rights reserved.
+//
+
+#import "SLTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLTAppDelegate class]));
+    }
+}
